7f37c352fae5802290dd251c3d9e782121e3d876
taccisum <liaojinfeng@deepexi.com>
2022-05-24 18:33:41 +0800

    feat: add dynamic class 'SayGoodBye.java' creation sample

--- src/main/java/com/github/tac/jagent/demo/Main.java
+++ src/main/java/com/github/tac/jagent/demo/Main.java
@@ -1,5 +1,7 @@
 package com.github.tac.jagent.demo;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 /**
@@ -11,10 +13,23 @@ public class Main {
         System.out.println("------ Main#main() args: " + Arrays.toString(args));
         sayhello();
         System.out.println("------ Main's classloader: " + Main.class.getClassLoader());
+        saygoodbye();
     }
 
     private static void sayhello() {
         // this statement will be replaced by MyAgent.java
         System.out.println("hello world!");
+    }
+
+    private static void saygoodbye() {
+        Class<?> clazz = null;
+        try {
+            clazz = Class.forName("com.github.tac.jagent.demo.SayGoodBye");
+            Object obj = clazz.newInstance();
+            Method dosay = clazz.getMethod("dosay", null);
+            dosay.invoke(obj);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/github/tac/jagent/demo/MyAgent.java
+++ src/main/java/com/github/tac/jagent/demo/MyAgent.java
@@ -17,8 +17,6 @@ import java.util.Map;
  * @since 2022/5/24
  */
 public class MyAgent {
-    static Map<ClassLoader, ClassPool> polls = new HashMap<>();
-
     /**
      * 该方法会在 main 方法之前被 jvm 调用
      * <br/>
@@ -32,33 +30,59 @@ public class MyAgent {
             @Override
             public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
                 String mainClass = "com.github.tac.jagent.demo.Main".replace(".", "/");
-                if (loader.toString().contains("AppClassLoader") && className.equals(mainClass)) {
-                    try {
-                        System.out.println("------ Loading class: " + mainClass);
-                        ClassPool pool = polls.get(loader);
-                        if (pool == null) {
-                            // instantiate javassit ClassPool for the class loader
-                            pool = new ClassPool(false);
-                            pool.insertClassPath(new LoaderClassPath(loader));
-                            polls.put(loader, pool);
+                if (loader.equals(MyAgent.class.getClassLoader())) {
+                    ClassPool pool = getClassPool(loader);
+
+                    if (className.equals(mainClass)) {
+                        try {
+                            System.out.println("------ Loading class: " + mainClass);
+                            // CtClass is mean compiled-time class
+                            CtClass c = pool.get(className.replace("/", "."));
+                            // modify body of method #sayhello()
+                            CtMethod m = c.getDeclaredMethod("sayhello");
+                            System.out.println("------ Modify body of #sayhello");
+                            m.setBody("System.out.println(\"hello javaagent. this content has modified by javassist!\");");
+                            // return modified bytecode
+                            return c.toBytecode();
+                        } catch (NotFoundException | IOException | CannotCompileException e) {
+                            // error happened. return origin bytecode
+                            e.printStackTrace();
+                            return classfileBuffer;
                         }
-                        // CtClass is mean compiled-time class
-                        CtClass c = pool.get(className.replace("/", "."));
-                        // modify body of method #sayhello()
-                        CtMethod m = c.getDeclaredMethod("sayhello");
-                        System.out.println("------ Modify body of #sayhello");
-                        m.setBody("System.out.println(\"hello javaagent. this content has modified by javassist!\");");
-                        // return modified bytecode
-                        return c.toBytecode();
-                    } catch (NotFoundException | IOException | CannotCompileException e) {
-                        // error happened. return origin bytecode
-                        e.printStackTrace();
-                        return classfileBuffer;
                     }
-                } else {
-                    return classfileBuffer;
                 }
+                return classfileBuffer;
             }
         });
+    }
+
+    static Map<ClassLoader, ClassPool> polls = new HashMap<>();
+
+    static ClassPool getClassPool(ClassLoader loader) {
+        ClassPool pool = polls.get(loader);
+        if (pool == null) {
+            // instantiate javassit ClassPool for the class loader
+            pool = new ClassPool(false);
+            pool.insertClassPath(new LoaderClassPath(loader));
+            polls.put(loader, pool);
+            // notify hooks
+            notifyPoolCreated(pool);
+        }
+        return pool;
+    }
+
+    static void notifyPoolCreated(ClassPool pool) {
+        if (pool.getClassLoader().equals(MyAgent.class.getClassLoader())) {
+            String className = "com.github.tac.jagent.demo.SayGoodBye";
+            System.out.println("------ Create Class " + className);
+            CtClass ctClass = pool.makeClass(className);
+            try {
+                CtMethod dosay = CtNewMethod.make("public void dosay() { System.out.println(\"byebye!\"); }", ctClass);
+                ctClass.addMethod(dosay);
+                ctClass.toClass();
+            } catch (CannotCompileException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
